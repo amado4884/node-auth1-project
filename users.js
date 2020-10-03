6902da23a0d46c1af88a13ec43ee6d1427bd8056
Amado Guerrero <amado4884@gmail.com>
2020-10-03 05:10:19 -0400

    mvp and one stretch

---
+++ users.js
@@ -0,0 +1,38 @@
+const knex = require("knex");
+const db = knex(require("./knexfile"));
+
+const find = async () => {
+  return await db("users");
+};
+
+const findById = async (id) => {
+  return await db("users").where({ id }).first();
+};
+
+const findByLogin = async (login) => {
+  return await db("users")
+    .where({ username: login })
+    .orWhere({ email: login })
+    .first();
+};
+
+const add = async (user) => {
+  return await db("users").insert(user);
+};
+
+const remove = async (id) => {
+  return await db("users").where({ id }).del();
+};
+
+const update = async (id, data) => {
+  return await db("users").where({ id }).first().update(data);
+};
+
+module.exports = {
+  find,
+  findById,
+  add,
+  remove,
+  update,
+  findByLogin,
+};
